28d0b5ba6094f77c5ac720f48213169204079b43
HarlowMoran <hmoran72@smith.edu>
2025-02-19 19:22:51 -0500

    Initial Commit

--- Engine.java
+++ Engine.java
@@ -1,3 +1,49 @@
-public class Engine {
+public class Engine implements EngineRequirements {
+    //Attributes
+    private FuelType f;
+    private double currentFuelLevel;
+    private double maxFuelLevel;
+    /**
+     * 
+     * @param f
+     * @param currentFuelLevel
+     * @param maxFuelLevel
+     */
 
+    public Engine(FuelType f, double currentFuelLevel, double maxFuelLevel) {
+        this.f = f;
+        this.currentFuelLevel = currentFuelLevel;
+        this.maxFuelLevel = maxFuelLevel;
+
+    }
+
+    public FuelType getFuelType() {
+        return this.f;
+    }
+
+    public double getMaxFuel() {
+        return 0.;
+    }
+
+    public double getCurrentFuel() {
+        return 0.;
+    }
+
+    public void refuel() {
+
+    }
+    
+    public Boolean go() {
+        return true;
+    }
+
+    public String toString() {
+        return("Engine has fuel type " + this.f + " current fuel level: " + this.currentFuelLevel + "and max fuel level: " + this.maxFuelLevel);
+    }
+
+    public static void main(String[] args) {
+        Engine myEngine = new Engine(FuelType.ELECTRIC, 0.0, 100.0);
+        System.out.println(myEngine);
+
+    }
 }
